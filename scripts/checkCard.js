d853fcbdc230f0f60b6e084cd7df1068a0b8c465
Michał Bysiewicz <bysiewicz.michal.kontakt@gmail.com>
2020-04-25 16:59:15 +0200

    Card manufacturer checking method
    export to test if it works

--- scripts/checkCard.js
+++ scripts/checkCard.js
@@ -2,6 +2,8 @@ class CheckCard{
     constructor(cardNumber){
         this.cardNumber = cardNumber;
         this.cardNumberLength = cardNumber.toString().length;
+        this.firstNumber = parseInt(cardNumber.toString().slice(0, 1));
+        this.firstTwoNumbers = parseInt(cardNumber.toString().slice(0, 2));
     }
     validLength(){
         if (this.cardNumberLength === 13 || this.cardNumberLength === 15 || this.cardNumberLength === 16) {
@@ -10,4 +12,17 @@ class CheckCard{
             return false;
         }
     }
+    whichCard(){
+        if (this.firstNumber === 4) {
+            return "Visa"
+        } else if (this.firstTwoNumbers >= 51 && this.firstTwoNumbers <= 55) {
+            return "Mastercard"
+        } else if (this.firstTwoNumbers === 34 || this.firstTwoNumbers === 37) {
+            return "American Express"
+        }else{
+            return false;
+        }
+    }
 }
+
+export default CheckCard;
