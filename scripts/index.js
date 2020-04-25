060a7f531cdb2b869b6006abddf4d270f977bfc5
Michał Bysiewicz <bysiewicz.michal.kontakt@gmail.com>
2020-04-25 15:44:10 +0200

    Add necessary variables

--- scripts/index.js
+++ scripts/index.js
@@ -2,4 +2,7 @@ const formDataCard = document.querySelector('#cardData');
 
 formDataCard.addEventListener('submit', (e)=>{
     e.preventDefault();
+
+    const cardNumber = parseInt(document.querySelector("#cardNumber").value);
+    const informationPlace = document.querySelector('#result');
 });
