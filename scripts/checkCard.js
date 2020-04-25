8b4faefcb975c47b1845a03db0be3055a0ad6e15
Michał Bysiewicz <bysiewicz.michal.kontakt@gmail.com>
2020-04-25 16:35:05 +0200

    Correct the mistake

--- scripts/checkCard.js
+++ scripts/checkCard.js
@@ -1,7 +1,7 @@
 class CheckCard{
     constructor(cardNumber){
         this.cardNumber = cardNumber;
-        this.cardNumberLength = toString(cardNumber).length;
+        this.cardNumberLength = cardNumber.toString().length;
     }
     validLength(){
         if (this.cardNumberLength === 13 || this.cardNumberLength === 15 || this.cardNumberLength === 16) {
