9242de5a692f3f3fd1f95d29ae4501f30ddd309e
Michał Bysiewicz <bysiewicz.michal.kontakt@gmail.com>
2020-04-25 16:13:20 +0200

    Add method which valid length of number

--- scripts/checkCard.js
+++ scripts/checkCard.js
@@ -1,5 +1,13 @@
 class CheckCard{
     constructor(cardNumber){
         this.cardNumber = cardNumber;
+        this.cardNumberLength = toString(cardNumber).length;
+    }
+    validLength(){
+        if (this.cardNumberLength === 13 || this.cardNumberLength === 15 || this.cardNumberLength === 16) {
+            return true;
+        }else{
+            return false;
+        }
     }
 }
