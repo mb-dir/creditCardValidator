76976cd62e9e294735bfdd1bafe6339babf527bd
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-07-17 18:12:59 +0200

    Add comments in index

--- scripts/index.js
+++ scripts/index.js
@@ -4,9 +4,11 @@ const formDataCard = document.querySelector('#cardData');
 formDataCard.addEventListener('submit', (e)=>{
     e.preventDefault();
 
+    //Variables realated with DOM
     const cardNumber = parseInt(document.querySelector("#cardNumber").value);
     const informationPlace = document.querySelector('#result');
 
+    //validCard handling
     const validator = new validCard(cardNumber);
     const resultValidation = validator.checkCardNumber();
 
